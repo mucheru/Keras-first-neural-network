7fa52e41f660cc31e3446e71b7a0a09b4a00b58d
steve <stephen@stephenmucheru79@gmail.com>
2024-05-02 12:18:41 +0300

    how to serialize model to JSON

--- keras_first_network.py
+++ keras_first_network.py
@@ -1,6 +1,7 @@
 from numpy import loadtxt
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
+import os
 dataset = loadtxt('pima-indians-diabetes.csv',delimiter= ',')
 X = dataset[:,0:8]
 y = dataset[:,8]
@@ -12,3 +13,10 @@ model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
 model.fit(X,y, epochs = 150,batch_size = 10)
 _, accuracy= model.evaluate(X,y)
 print('Accuracy: %.2f' % (accuracy*100))
+#serialize model to Json
+model_json = model.to_json()
+with open("model.json","w") as json_file:
+    json_file.write(model_json)
+# serialize weights to HDF5
+model.save_weights("model.h5")
+print("Saved model to disk")
